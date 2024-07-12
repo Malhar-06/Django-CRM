6eecfd87d2d9b1f8902bdab9c14051258a934cac
Malhar-06 <elixir0malhar@gmail.com>
2024-07-12 17:31:40 +0530

    navbar

--- website/views.py
+++ website/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html', {})
 
-def base(request):
-    return render(request, 'base.html', {})
+# def base(request):
+#     return render(request, 'base.html', {})
