5cdb33526739e34b23e1bea8cec9b88517c63b49
Malhar-06 <elixir0malhar@gmail.com>
2024-07-12 21:10:51 +0530

    Register

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home' ),
-    # path('base', views.base, name='base' ),
+    # path('login/', views.login_user, name='login' ),
+    path('logout/', views.logout_user, name='logout' ),
+    path('register/', views.register_user, name='register' ),
 ]
